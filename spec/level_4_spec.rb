fa020417c9ca01bfbee658637b89632b32cd035d
changamanda <amanda.chang10@gmail.com>
2015-05-28 14:38:44 -0400

    Consistent format

--- spec/level_4_spec.rb
+++ spec/level_4_spec.rb
@@ -16,19 +16,19 @@ describe "Hashes" do
     end
   end
 
-  describe 'add_knight(name, quest, favorite_color, hash)' do
+  describe '#add_knight(name, quest, favorite_color, hash)' do
     it 'adds a knight to the hash' do
       expect(add_knight("Ben", "To tell the best jokes", "Blue! No, yellow...", adventure_hash)[:knights]).to include({:name=>"Ben", :quest=>"To tell the best jokes", :favorite_color=>"Blue! No, yellow..."})
     end
   end
 
-  describe 'all_quests(adventure_hash)' do
+  describe '#all_quests(adventure_hash)' do
     it 'returns an array of all quests' do
       expect(all_quests(adventure_hash)).to eq(["To conquer the world, one Ruby method at a time", "To own every T-shirt in the universe", "To kill them with kindness", "To share cute kitten videos", "To spread AJAX wisdom to the coding world", "To be a coding wizard"])
     end
   end
 
-  describe 'reformat_hash(adventure_hash)' do
+  describe '#reformat_hash(adventure_hash)' do
     it "returns a hash with knights' names as keys and favorite colors as values" do
       expect(reformat_hash(adventure_hash)).to eq({"Amanda"=>"Blue", "Steven"=>"Red", "Josh"=>"Orange", "Sophie"=>"Green", "Rose"=>"Purple", "Deniz"=>"Yellow"})
     end
